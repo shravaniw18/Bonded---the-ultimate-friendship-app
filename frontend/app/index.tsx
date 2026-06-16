c498088374b50bab86bf8f4377d5b608df8af716
shravaniw18 <shr150607@gmail.com>
2026-06-16 15:43:27 +0530

    changes

--- frontend/app/index.tsx
+++
@@ -1,9 +0,0 @@
-// frontend/app/index.tsx
-// Root redirect element that redirects unauthenticated users to /login
-
-import React from 'react';
-import { Redirect } from 'expo-router';
-
-export default function IndexRedirect() {
-  return <Redirect href="/login" />;
-}
